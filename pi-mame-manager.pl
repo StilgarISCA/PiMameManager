e23434a3bcb031021892d858a309f8a8c928891f
“Glenn <glenn.hoeppner@yakhair.com>
2016-06-01 23:18:13 -0400

    Add is mame running check

--- pi-mame-manager.pl
+++ pi-mame-manager.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 
 my $is_up = IsEthernetUp();
-
+my $is_mame_running = IsMameRunning();
 return 0;
 
 ### End Main Body ###
@@ -11,13 +11,23 @@ return 0;
 #
 # Determine if the physical ethernet adapter is up (or not)
 #
-# Returns 1 (true) if ethernet functional, false otherwise
+# Returns non-zero (true) if ethernet functional, false otherwise
 #
 sub IsEthernetUp()
 {
   my $ethernet_response = `cat /sys/class/net/eth2/operstate`;
   chomp( $ethernet_response );
   return ($ethernet_response eq 'up');
+}
+
+#
+# Determine if Mame is running (or not)
+#
+# Returns non-zero (true) if mame running, false otherwise
+#
+sub IsMameRunning()
+{
+  return `pidof mame`;
 }
 
 #EOF
