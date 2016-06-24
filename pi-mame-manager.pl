ac45cdb2659b3c78bec330bb6903192283252c92
Glenn Hoeppner <g.hoeppner@techsmith.com>
2016-06-24 07:31:45 -0400

    Use the right string delimiters

--- pi-mame-manager.pl
+++ pi-mame-manager.pl
@@ -90,7 +90,7 @@ sub StartMame()
 #
 sub UpdateLastPoweredRunTime()
 {
-  system( 'touch /home/$USER/.lastpoweredrun' );
+  system( "touch /home/$USER/.lastpoweredrun" );
 }
 
 #
@@ -98,7 +98,7 @@ sub UpdateLastPoweredRunTime()
 #
 sub UpdateLastUnpoweredRunTime()
 {
-  system( 'touch /home/$USER/.lastunpoweredrun' );
+  system( "touch /home/$USER/.lastunpoweredrun" );
 }
 
 ### Start Main Program ###
